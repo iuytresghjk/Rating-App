0a4eefeca637ca646acb881a4f1362d4a0bb9b6d
Farhan Riaz <jam34646@gmail.com>
2025-07-15 12:56:45 +0500

    Added Firebase

--- app/src/main/java/com/example/fhananfarhan/CommentItem.java
+++ app/src/main/java/com/example/fhananfarhan/CommentItem.java
@@ -1,11 +1,37 @@
 package com.example.fhananfarhan;
 
-public class CommentItem {
-    public float rating;
-    public String comment;
+import com.google.firebase.Timestamp;
 
-    public CommentItem(float rating, String comment) {
-        this.rating = rating;
+public class CommentItem {
+    private String comment;
+    private float rating;
+    private String email;
+    private Timestamp timestamp;
+
+    public CommentItem() {
+        // required by Firebase
+    }
+
+    public CommentItem(String comment, float rating, String email) {
         this.comment = comment;
+        this.rating = rating;
+        this.email = email;
+        this.timestamp = null; // Firestore will set it server-side
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
     }
 }
